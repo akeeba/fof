4cbdc044cd3d2d9d2982c31e92edd06a7a878643
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-10-15 08:56:00 +0300

    + Support for Joomla 4 centralized Download Key management of paid extensions in its Update Sites page
    Backported from FOF 3

--- fof/Update/Update.php
+++ fof/Update/Update.php
@@ -7,10 +7,12 @@
 
 namespace FOF40\Update;
 
+use Exception;
 use FOF40\Container\Container;
 use FOF40\Model\Model;
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Updater\Updater;
+use Joomla\CMS\Component\ComponentHelper as JComponentHelper;
+use Joomla\CMS\Updater\Updater as JUpdater;
+use SimpleXMLElement;
 
 defined('_JEXEC') or die;
 
@@ -19,7 +21,7 @@ defined('_JEXEC') or die;
  */
 class Update extends Model
 {
-	/** @var Updater The Joomla! updater object */
+	/** @var JUpdater The Joomla! updater object */
 	protected $updater = null;
 
 	/** @var int The extension_id of this component */
@@ -40,6 +42,9 @@ class Update extends Model
 	/** @var string The extra query to append to (commercial) components' download URLs */
 	protected $extraQuery = null;
 
+	/** @var string The component Options key which stores a copy of the Download ID */
+	protected $paramsKey = 'update_dlid';
+
 	/**
 	 * Public constructor. Initialises the protected members as well. Useful $config keys:
 	 * update_component        The component name, e.g. com_foobar
@@ -47,17 +52,23 @@ class Update extends Model
 	 * update_site            The URL to the component's update XML stream
 	 * update_extraquery    The extra query to append to (commercial) components' download URLs
 	 * update_sitename        The update site's name (description)
+	 * update_paramskey        The component parameters key which holds the license key in J3 (and a copy of it in J4)
 	 *
-	 * @param array $config
+	 * @param   array  $config
 	 */
 	public function __construct(array $config = [])
 	{
 		$container = Container::getInstance('com_FOOBAR');
 
+		if (isset($config['update_container']) && is_object($config['update_container']) && ($config['update_container'] instanceof Container))
+		{
+			$container = $config['update_container'];
+		}
+
 		parent::__construct($container);
 
 		// Get an instance of the updater class
-		$this->updater = Updater::getInstance();
+		$this->updater = JUpdater::getInstance();
 
 		// Get the component name
 		if (isset($config['update_component']))
@@ -93,6 +104,12 @@ class Update extends Model
 			$this->updateSiteName = $config['update_sitename'];
 		}
 
+		// Get the extra query
+		if (isset($config['update_paramskey']))
+		{
+			$this->paramsKey = $config['update_paramskey'];
+		}
+
 		// Get the extension type
 		$extension = $this->getExtensionObject();
 
@@ -114,13 +131,245 @@ class Update extends Model
 	}
 
 	/**
+	 * Gets the license key for a paid extension.
+	 *
+	 * On Joomla! 3 or when $forceLegacy is true we look in the component Options.
+	 *
+	 * On Joomla! 4 we use the information in the dlid element of the extension's XML manifest to parse the extra_query
+	 * fields of all configured update sites of the extension. This is the same thing Joomla does when it tries to
+	 * determine the license key of our extension when installing updates. If the extension is missing, it has no
+	 * associated update sites, the update sites are missing / rebuilt / disassociated from the extension or the
+	 * extra_query of all update site records is empty we parse the $extraQuery set in the constructor, if any. Also
+	 * note that on Joomla 4 mode if the extension does not exist, does not have a manifest or does not have a valid
+	 * dlid element in its manifest we will end up returning an empty string, just like Joomla! itself would have done
+	 * when installing updates.
+	 *
+	 * @param   bool  $forceLegacy  Should I always retrieve the legacy license key, even in J4?
+	 *
+	 * @return  string
+	 */
+	public function getLicenseKey(bool $forceLegacy = false): string
+	{
+		$legacyParamsKey = $this->getLegacyParamsKey();
+
+		// Joomla 3 (Legacy): Download ID stored in the component options
+		if ($forceLegacy || !version_compare(JVERSION, '3.999.999', 'gt'))
+		{
+			return $this->container->params->get($legacyParamsKey, '');
+		}
+
+		// Joomla! 4. We need to parse the extra_query of the update sites to get the correct Download ID.
+		$updateSites = $this->getUpdateSites();
+		$extra_query = array_reduce($updateSites, function ($extra_query, $updateSite) {
+			if (!empty($extra_query))
+			{
+				return $extra_query;
+			}
+
+			return $updateSite['extra_query'];
+		}, '');
+
+		// Fall back to legacy extra query
+		if (empty($extra_query))
+		{
+			$extra_query = $this->extraQuery;
+		}
+
+		// Return the parsed results.
+		return $this->getLicenseKeyFromExtraQuery($extra_query);
+	}
+
+	/**
+	 * Get the contents of all the update sites of the configured extension
+	 *
+	 * @return  array|null
+	 */
+	public function getUpdateSites(): ?array
+	{
+		$updateSiteIDs = $this->getUpdateSiteIds();
+		$db    = $this->container->db;
+		$query = $db->getQuery(true)
+			->select('*')
+			->from($db->qn('#__update_sites'))
+			->where($db->qn('update_site_id') . ' IN (' . implode(', ', $updateSiteIDs) . ')');
+
+		try
+		{
+			$db->setQuery($query);
+
+			$ret = $db->loadAssocList('update_site_id');
+		}
+		catch (Exception $e)
+		{
+			$ret = null;
+		}
+
+		return empty($ret) ? [] : $ret;
+	}
+
+	public function setLicenseKey(string $licenseKey)
+	{
+		$legacyParamsKey = $this->getLegacyParamsKey();
+
+		// Sanitize and validate the license key. If it's not valid we set an empty license key.
+		$licenseKey = $this->sanitizeLicenseKey($licenseKey);
+
+		if (!$this->isValidLicenseKey($licenseKey))
+		{
+			$licenseKey = '';
+		}
+
+		// Update $this->extraQuery.
+		$this->extraQuery = $this->getExtraQueryString($licenseKey);
+
+		// Save the license key in the component options ($legacyParamsKey)
+		$this->container->params->set($legacyParamsKey, $licenseKey);
+		$this->container->params->save();
+
+		// Apply the new extra_query to the update site
+		$this->refreshUpdateSite();
+	}
+
+	/**
+	 * Copies a Joomla 3 license key from the Options storage to Joomla 4 download key storage (the extra_query column
+	 * of the #__update_sites table).
+	 *
+	 * This method does nothing on Joomla 3.
+	 *
+	 * @return  void
+	 */
+	public function upgradeLicenseKey(): void
+	{
+		// Only applies to Joomla! 4
+		if (!version_compare(JVERSION, '3.999.999', 'gt'))
+		{
+			return;
+		}
+
+		// Make sure we DO have a legacy license key
+		$legacyKey = $this->getLicenseKey(true);
+
+		if (empty($legacyKey))
+		{
+			return;
+		}
+
+		// Make sure we DO NOT have a J4 key. If we do, the J4 key wins and gets backported to legacy storage.
+		$licenseKey = $this->getLicenseKey(false);
+
+		if (!empty($licenseKey))
+		{
+			$this->backportLicenseKey();
+
+			return;
+		}
+
+		// Save the legacy key as non-legacy. This updates the #__update_sites record, applying the license key.
+		$this->setLicenseKey($legacyKey);
+	}
+
+	/**
+	 * Copies a Joomla 4 license key from the download key storage (the extra_query column of the #__update_sites table)
+	 * to the legacy Options storage.
+	 *
+	 * This method does nothing on Joomla 3.
+	 *
+	 * @return  void
+	 */
+	public function backportLicenseKey(): void
+	{
+		$legacyParamsKey = $this->getLegacyParamsKey();
+
+		// Only applies to Joomla! 4
+		if (!version_compare(JVERSION, '3.999.999', 'gt'))
+		{
+			return;
+		}
+
+		// Make sure we DO have a J4 key
+		$licenseKey = $this->getLicenseKey(false);
+
+		if (empty($licenseKey))
+		{
+			return;
+		}
+
+		// Make sure that the legacy key is NOT the same as the J4 key
+		$legacyKey = $this->getLicenseKey(true);
+
+		if ($legacyKey == $licenseKey)
+		{
+			return;
+		}
+
+		// Save the license key to the legacy storage (component options)
+		$this->container->params->set($legacyParamsKey, $licenseKey);
+		$this->container->params->save();
+	}
+
+	/**
+	 * Get an extra query string based on the dlid element of the XML manifest file of the extension.
+	 *
+	 * If the extension does not exist, the manifest does not exist or it does not have a dlid element we fall back to
+	 * the legacy implementation of extra_query (getExtraQueryStringLegacy)
+	 *
+	 * @param   string  $licenseKey
+	 *
+	 * @return  string
+	 */
+	public function getExtraQueryString(string $licenseKey): string
+	{
+		// Make sure the (sanitized) license key is valid. Otherwise we return an empty string.
+		$licenseKey = $this->sanitizeLicenseKey($licenseKey);
+
+		if (!$this->isValidLicenseKey($licenseKey))
+		{
+			return '';
+		}
+
+		// Get a fallback extra query using the legacy method
+		$fallbackExtraQuery = $this->getExtraQueryStringLegacy($licenseKey);
+
+		// Get the extension XML manifest. If the extension or the manifest don't exist use the fallback extra_query.
+		$extension = $this->getExtensionObject();
+
+		if (!$extension)
+		{
+			return $fallbackExtraQuery;
+		}
+
+		$installXmlFile = $this->getManifestXML(
+			$extension->element,
+			$extension->type,
+			(int) $extension->client_id,
+			$extension->folder
+		);
+
+		if (!$installXmlFile)
+		{
+			return $fallbackExtraQuery;
+		}
+
+		// If the manifest does not have the dlid element return the fallback extra_query.
+		if (!isset($installXmlFile->dlid))
+		{
+			return $fallbackExtraQuery;
+		}
+
+		$prefix = (string) $installXmlFile->dlid['prefix'];
+		$suffix = (string) $installXmlFile->dlid['suffix'];
+
+		return $prefix . $this->sanitizeLicenseKey($licenseKey) . $suffix;
+	}
+
+	/**
 	 * Retrieves the update information of the component, returning an array with the following keys:
 	 *
-	 * hasUpdate    True if an update is available
-	 * version        The version of the available update
-	 * infoURL        The URL to the download page of the update
+	 * hasUpdate  True if an update is available
+	 * version    The version of the available update
+	 * infoURL    The URL to the download page of the update
 	 *
-	 * @param bool $force Set to true if you want to forcibly reload the update information
+	 * @param   bool  $force  Set to true if you want to forcibly reload the update information
 	 *
 	 * @return  array  See the method description for more information
 	 */
@@ -175,7 +424,7 @@ class Update extends Model
 		}
 
 		// Use the update cache timeout specified in com_installer
-		$comInstallerParams = ComponentHelper::getParams('com_installer', false);
+		$comInstallerParams = JComponentHelper::getParams('com_installer', false);
 		$timeout            = 3600 * $comInstallerParams->get('cachetimeout', '6');
 
 		// Load any updates from the network into the #__updates table
@@ -205,9 +454,9 @@ class Update extends Model
 	/**
 	 * Gets the update site Ids for our extension.
 	 *
-	 * @return    array|null    An array of Ids or null if the query failed.
+	 * @return    array    An array of IDs
 	 */
-	public function getUpdateSiteIds(): ?array
+	public function getUpdateSiteIds(): array
 	{
 		$db    = $this->container->db;
 		$query = $db->getQuery(true)
@@ -215,9 +464,17 @@ class Update extends Model
 			->from($db->qn('#__update_sites_extensions'))
 			->where($db->qn('extension_id') . ' = ' . $db->q($this->extension_id));
 		$db->setQuery($query);
-		$updateSiteIds = $db->loadColumn(0);
 
-		return $updateSiteIds;
+		try
+		{
+			$ret = $db->loadColumn(0);
+		}
+		catch (Exception $e)
+		{
+			$ret = null;
+		}
+
+		return is_array($ret) ? $ret : [];
 	}
 
 	/**
@@ -233,7 +490,7 @@ class Update extends Model
 	/**
 	 * Override the currently installed version as reported by the #__extensions table
 	 *
-	 * @param string $version
+	 * @param   string  $version
 	 */
 	public function setVersion(string $version): void
 	{
@@ -474,9 +731,9 @@ class Update extends Model
 	/**
 	 * Returns an object with the #__extensions table record for the current extension.
 	 *
-	 * @return  object
+	 * @return  object|null
 	 */
-	private function getExtensionObject()
+	public function getExtensionObject()
 	{
 		list($extensionPrefix, $extensionName) = explode('_', $this->component);
 
@@ -501,9 +758,186 @@ class Update extends Model
 			->from($db->qn('#__extensions'))
 			->where($db->qn('type') . ' = ' . $db->q($type))
 			->where($db->qn('element') . ' = ' . $db->q($name));
-		$db->setQuery($query);
-		$extension = $db->loadObject();
+
+		try
+		{
+			$db->setQuery($query);
+			$extension = $db->loadObject();
+		}
+		catch (Exception $e)
+		{
+			return null;
+		}
 
 		return $extension;
+	}
+
+	/**
+	 * Is the provided string a valid license key?
+	 *
+	 * YOU SHOULD OVERRIDE THIS METHOD. The default implementation checks for valid Download IDs in the format used by
+	 * Akeeba software.
+	 *
+	 * @param   string  $licenseKey
+	 *
+	 * @return  bool
+	 */
+	public function isValidLicenseKey(string $licenseKey): bool
+	{
+		return preg_match('/^([0-9]{1,}:)?[0-9a-f]{32}$/i', $licenseKey) === 1;
+	}
+
+	/**
+	 * Sanitizes the license key.
+	 *
+	 * YOU SHOULD OVERRIDE THIS METHOD. The default implementation returns a lowercase string with all characters except
+	 * letters, numbers and colons removed.
+	 *
+	 * @param   string  $licenseKey
+	 *
+	 * @return  string  The sanitized license key
+	 */
+	public function sanitizeLicenseKey(string $licenseKey): string
+	{
+		return strtolower(preg_replace("/[^a-zA-Z0-9:]/", "", $licenseKey));
+	}
+
+	/**
+	 * Returns the component Options key which holds a copy of the license key
+	 *
+	 * @return  string
+	 */
+	protected function getLegacyParamsKey(): string
+	{
+		if (!empty($this->paramsKey))
+		{
+			return $this->paramsKey;
+		}
+
+		$this->paramsKey = 'update_dlid';
+
+		return $this->paramsKey;
+	}
+
+	/**
+	 * Extract the download ID from an extra_query based on the prefix and suffix information stored in the dlid element
+	 * of the extension's XML manifest file.
+	 *
+	 * @param   string  $extra_query
+	 *
+	 * @return string
+	 */
+	protected function getLicenseKeyFromExtraQuery(string $extra_query): string
+	{
+		$extra_query = trim($extra_query);
+
+		if (empty($extra_query))
+		{
+			return '';
+		}
+
+		// Get the extension XML manifest. If the extension or the manifest don't exist return an empty string.
+		$extension = $this->getExtensionObject();
+
+		if (!$extension)
+		{
+			return '';
+		}
+
+		$installXmlFile = $this->getManifestXML(
+			$extension->element,
+			$extension->type,
+			(int) $extension->client_id,
+			$extension->folder
+		);
+
+		if (!$installXmlFile)
+		{
+			return '';
+		}
+
+		// If the manifest does not have a dlid element return an empty string.
+		if (!isset($installXmlFile->dlid))
+		{
+			return '';
+		}
+
+		// Naive parsing of the extra_query, the same way Joomla does.
+		$prefix     = (string) $installXmlFile->dlid['prefix'];
+		$suffix     = (string) $installXmlFile->dlid['suffix'];
+		$licenseKey = substr($extra_query, strlen($prefix));
+
+		if ($licenseKey === false)
+		{
+			return '';
+		}
+
+		if ($suffix)
+		{
+			$licenseKey = substr($licenseKey, 0, -strlen($suffix));
+		}
+
+		return ($licenseKey === false) ? '' : $licenseKey;
+	}
+
+	/**
+	 * Get a legacy extra query string. Do NOT call this directly. Call getExtraQueryString() instead.
+	 *
+	 * YOU SHOULD OVERRIDE THIS METHOD. This returns dlid=SANITIZED_LICENSE_KEY which is what Akeeba Release System,
+	 * used to deliver all Akeeba extensions, expects.
+	 *
+	 * @param   string  $licenseKey  The license key
+	 *
+	 * @return  string  The extra_query string to append to a downlaod URL to implement the license key
+	 */
+	protected function getExtraQueryStringLegacy(string $licenseKey): string
+	{
+		if (empty($licenseKey) || !$this->isValidLicenseKey($licenseKey))
+		{
+			return '';
+		}
+
+		return 'dlid=' . $this->sanitizeLicenseKey($licenseKey);
+	}
+
+	/**
+	 * Get the manifest XML file of a given extension.
+	 *
+	 * @param   string   $element    element of an extension
+	 * @param   string   $type       type of an extension
+	 * @param   integer  $client_id  client_id of an extension
+	 * @param   string   $folder     folder of an extension
+	 *
+	 * @return  SimpleXMLElement
+	 */
+	protected function getManifestXML(string $element, string $type, int $client_id = 1, ?string $folder = null): SimpleXMLElement
+	{
+		$path = $client_id ? JPATH_ADMINISTRATOR : JPATH_ROOT;
+
+		switch ($type)
+		{
+			case 'component':
+				$path .= '/components/' . $element . '/' . substr($element, 4) . '.xml';
+				break;
+			case 'plugin':
+				$path .= '/plugins/' . $folder . '/' . $element . '/' . $element . '.xml';
+				break;
+			case 'module':
+				$path .= '/modules/' . $element . '/' . $element . '.xml';
+				break;
+			case 'template':
+				$path .= '/templates/' . $element . '/templateDetails.xml';
+				break;
+			case 'library':
+				$path = JPATH_ADMINISTRATOR . '/manifests/libraries/' . $element . '.xml';
+				break;
+			case 'file':
+				$path = JPATH_ADMINISTRATOR . '/manifests/files/' . $element . '.xml';
+				break;
+			case 'package':
+				$path = JPATH_ADMINISTRATOR . '/manifests/packages/' . $element . '.xml';
+		}
+
+		return simplexml_load_file($path);
 	}
 }
