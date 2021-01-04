1d6de9f7d4a486055868f5765b6284d597affe2a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-04 11:10:34 +0200

    Updated copyright. Happy New Year!

--- Tests/_data/fake_test_template/media/com_foobar/js/test.js
+++ Tests/_data/fake_test_template/media/com_foobar/js/test.js
@@ -1,7 +1,7 @@
 /*
  * @package   FOF
- * @copyright Copyright (c)2010-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license   GNU General Public License version 3, or later
+ * @copyright Copyright (c)2010-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 2, or later
  */
 
 /* Only used for testing */
