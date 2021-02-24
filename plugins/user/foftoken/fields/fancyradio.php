0818c416f300ae4f4388cb1b6beb36e922b05bab
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-02-24 14:15:07 +0200

    Joomla XML field: fancyradio
    Update the FOF Token user plugin to use it

---
+++ plugins/user/foftoken/fields/fancyradio.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package   FOF
+ * @copyright Copyright (c)2010-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 2, or later
+ */
+
+defined('_JEXEC') || die();
+
+require_once JPATH_LIBRARIES . '/fof40/Html/Fields/fancyradio.php';
